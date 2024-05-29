f822ac967bef39dc204c8680e1a2e223f0f996ff
Yashvitha <kyashvitha@gmail.com>
2024-05-29 18:00:54 +0200

    feat(pages): add nav to signup page and update the style

--- wmc/src/lib/ui/signup-form.tsx
+++ wmc/src/lib/ui/signup-form.tsx
@@ -17,7 +17,7 @@ export function SignupFormDemo() {
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Welcome to Aceternity
+        Welcome to WriteMe
       </h2>
       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
         Login to aceternity if you can because we don&apos;t have a login flow
