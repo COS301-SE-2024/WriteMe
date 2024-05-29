c0472c239742dea3aefaf4cde994bbae6d00689b
Yashvitha <kyashvitha@gmail.com>
2024-05-29 20:13:33 +0200

    feat(pages): add basic profile to the profile page

--- apps/writeme/app/user/[username]/page.tsx
+++ apps/writeme/app/user/[username]/page.tsx
@@ -1,6 +1,7 @@
 import WriteMeLogo from '../../../assets/WriteMe.png';
 import Image from 'next/image';
 import Profile from '../../../assets/profile.jpg';
+import { Button } from '@writeme/wmc/lib/ui/button';
 
 /* eslint-disable-next-line */
 export interface UserProps {}
@@ -16,6 +17,17 @@ export default function User(props: UserProps) {
           <Image src={Profile} alt="Profile Image" layout="fill" objectFit="contain" />
         </div>
       </nav>
+
+      {/* User Profile Section */}
+      <div className="flex flex-col items-start p-10">
+        <div className="relative w-80 h-80 mb-1"> 
+          <Image src={Profile} alt="User Profile Image" layout="fill" objectFit="contain" />
+        </div>
+        <h1 className="text-3xl font-bold mx-12">Username</h1>
+        <p className="text-lg text-gray-500 mx-12 mb-4">Short bio or description about the user.</p>
+        <Button className='mx-12'>Edit profile</Button>
+        
+      </div>
     </div>
   );
 }
