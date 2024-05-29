d1c3834f1cacc763fed758e863d0e1127a35d933
Yashvitha <kyashvitha@gmail.com>
2024-05-29 19:58:44 +0200

    feat(pages): add nav bar to the profile page

--- apps/writeme/app/user/[username]/page.tsx
+++ apps/writeme/app/user/[username]/page.tsx
@@ -1,12 +1,21 @@
-import styles from './page.module.css';
+import WriteMeLogo from '../../../assets/WriteMe.png';
+import Image from 'next/image';
+import Profile from '../../../assets/profile.jpg';
 
 /* eslint-disable-next-line */
 export interface UserProps {}
 
 export default function User(props: UserProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Myworks!</h1>
+    <div className="flex flex-col h-screen">
+      <nav className="w-full flex justify-between items-center p-4">
+        <div className="relative w-60 h-20 -mx-10">
+          <Image src={WriteMeLogo} alt="WriteMe Logo" layout="fill" objectFit="contain" />
+        </div>
+        <div className="relative w-12 h-12 mx-4">
+          <Image src={Profile} alt="Profile Image" layout="fill" objectFit="contain" />
+        </div>
+      </nav>
     </div>
   );
 }
