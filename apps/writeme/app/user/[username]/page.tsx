32d1a8c52598bc25608fb4a5b5c7569e6e9f4864
Yashvitha <kyashvitha@gmail.com>
2024-05-29 21:23:47 +0200

    feat(pages): basic styling of the profile page

--- apps/writeme/app/user/[username]/page.tsx
+++ apps/writeme/app/user/[username]/page.tsx
@@ -2,6 +2,7 @@ import WriteMeLogo from '../../../assets/WriteMe.png';
 import Image from 'next/image';
 import Profile from '../../../assets/profile.jpg';
 import { Button } from '@writeme/wmc/lib/ui/button';
+import { IconUser, IconMail } from '@tabler/icons-react';
 
 /* eslint-disable-next-line */
 export interface UserProps {}
@@ -13,6 +14,20 @@ export default function User(props: UserProps) {
         <div className="relative w-60 h-20 -mx-10">
           <Image src={WriteMeLogo} alt="WriteMe Logo" layout="fill" objectFit="contain" />
         </div>
+
+        <ul className="flex space-x-8">
+          <li>
+          <a href="#overview" className="text-black hover:text-purple-200 relative after:content-[''] after:block after:w-full after:h-1 after:bg-[#a2a8f0] after:mt-1 after:rounded-full">
+              Overview
+            </a>
+          </li>
+          <li>
+            <a href="#stories" className="text-black hover:text-purple-200">
+              Stories
+            </a>
+          </li>
+        </ul>
+
         <div className="relative w-12 h-12 mx-4">
           <Image src={Profile} alt="Profile Image" layout="fill" objectFit="contain" />
         </div>
@@ -25,8 +40,19 @@ export default function User(props: UserProps) {
         </div>
         <h1 className="text-3xl font-bold mx-12">Username</h1>
         <p className="text-lg text-gray-500 mx-12 mb-4">Short bio or description about the user.</p>
-        <Button className='mx-12'>Edit profile</Button>
-        
+        <Button className='mx-12 mb-3'>Edit profile</Button>
+        <div className='flex mx-12 mb-3'>
+          <IconUser />
+          <a className='font-bold mx-2'>1 </a>
+          <a className='text-gray-500'>follower .</a>
+          <a className='font-bold mx-2'>2 </a>
+          <a className='text-gray-500'>following</a>
+        </div>
+        <div className="border-t border-gray-300 my-5 mx-12 w-1/5"></div>
+        <div className='flex mx-10'>
+          <IconMail className='mx-2' />
+          <a>user@gmail.com</a>
+        </div>
       </div>
     </div>
   );
