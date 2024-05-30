0af6082b4248ba7d87e4a2a9730d0284a938c5da
Khyal Kara <khyal@khyalkara.com>
2024-05-30 21:04:54 +0200

    feat(components): working storybook config

--- wmc/.storybook/main.ts
+++ wmc/.storybook/main.ts
@@ -3,16 +3,39 @@ import type { StorybookConfig } from '@storybook/react-vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { mergeConfig } from 'vite';
 
+import { URL, fileURLToPath} from 'node:url';
+
+
 const config: StorybookConfig = {
   stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  core: {
+    builder: '@storybook/builder-vite'
+  },
   framework: {
-    name: '@storybook/react-vite',
+    // name: '@storybook/react-vite',
+    name: '@storybook/nextjs',
     options: {},
+  },
+  typescript: {
+    // Enables the `react-docgen-typescript` parser.
+    // See https://storybook.js.org/docs/api/main-config-typescript for more information about this option.
+    reactDocgen: 'react-docgen-typescript',
   },
 
   viteFinal: async (config) =>
     mergeConfig(config, {
+      define: {
+        'process.env.NODE_DEBUG': false,
+      },
+      resolve: {
+        alias: [
+          {
+            find: "@writeme/wmc/",
+            replacement: fileURLToPath(new URL("../src/", import.meta.url))
+          }
+        ]
+      },
       plugins: [nxViteTsPaths()],
     }),
 };
--- wmc/.storybook/preview.ts
+++ wmc/.storybook/preview.ts
@@ -1,1 +1,15 @@
 import "../../apps/writeme/app/global.css";
+
+import { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  // ...
+  parameters: {
+    // ...
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
+
+export default preview;
