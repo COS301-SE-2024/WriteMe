62f6e232ea11c43eb37aff5ede2f16cf68a9c81d
Lucasb0202 <u21436135@tuks.co.za>
2024-06-07 20:04:43 +0200

    feat(page): edit profile integration

--- apps/writeme/app/user/[username]/edit-profile/page.tsx
+++ apps/writeme/app/user/[username]/edit-profile/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import EditProfileForm from './EditProfileForm'
+import LocalNavbar from '@writeme/wmc/lib/ui/local-navbar';
 
 const EditProfile = () => {
   return (
-    <EditProfileForm />
+    <>
+      <LocalNavbar />
+      <EditProfileForm />
+    </>
   )
 }
 
