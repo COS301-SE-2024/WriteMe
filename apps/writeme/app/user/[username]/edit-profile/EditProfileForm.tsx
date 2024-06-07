540bcf6bb3c2e4c501d7ca3f374403385c9b7901
Lucasb0202 <u21436135@tuks.co.za>
2024-06-07 20:28:34 +0200

    feat(page): edit profile integration

--- apps/writeme/app/user/[username]/edit-profile/EditProfileForm.tsx
+++ apps/writeme/app/user/[username]/edit-profile/EditProfileForm.tsx
@@ -5,34 +5,22 @@ import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { updateUserSchema, UpdateUserInput } from '../../../../db/user-schema';
 import { useToast } from '@writeme/wmc/lib/ui/use-toast';
 import AutoForm, { AutoFormSubmit } from '../../../../../../wmc/src/lib/ui/auto-form'
 
 const EditProfileForm = () => {
   return (
-    // name: string({ required_error: 'Name is required' }).min(
-    //   1,
-    //   'Name is required'
-    // ),
-    // email: string({ required_error: 'Email is required' })
-    //   .min(1, 'Email is required')
-    //   .email('Invalid email'),
-    // photo: string().optional()
     <Card className="mx-auto max-w-sm mt-8">
       <CardHeader>
           <CardTitle className="text-2xl">Edit your profile</CardTitle>
         </CardHeader>
       <CardContent>
-        <div className='grid gap-4'>
           <AutoForm
-            className='flex flex-col gap-2'
             formSchema={updateUserSchema}
             >
             <AutoFormSubmit>Update profile</AutoFormSubmit>
           </AutoForm>
-        </div>
       </CardContent>
     </Card>
   )
