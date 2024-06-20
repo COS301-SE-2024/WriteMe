9121837e6fc51008d7d99de367e91f6f73cff386
Yashvitha <kyashvitha@gmail.com>
2024-06-20 21:29:06 +0200

    fix(components): refactor commentsection component

--- apps/writeme/components/comments-sections.tsx
+++ apps/writeme/components/comments-sections.tsx
@@ -1,84 +1,157 @@
-"use client";
+'use client';
 
-import React from "react";
-
-import Link from "next/link";
-
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@writeme/wmc/lib/ui/card";
-
-  import {
-    Avatar, 
-    AvatarImage,
-    AvatarFallback
-} from "@writeme/wmc/lib/ui/avatar";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import EmojiPicker from 'emoji-picker-react';
 
 import {
-    Input
-} from "@writeme/wmc/lib/ui/input";
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@writeme/wmc/lib/ui/card';
 
 import {
-    buttonVariants,
-    Button
-} from "@writeme/wmc/lib/ui/button";
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+} from '@writeme/wmc/lib/ui/avatar';
 
+import { Input } from '@writeme/wmc/lib/ui/input';
+
+import { buttonVariants, Button } from '@writeme/wmc/lib/ui/button';
 
 import { MessageSquare, Smile, Send } from 'lucide-react';
 import { cn } from '@writeme/wmc/utils';
-import * as dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import { Separator } from "@writeme/wmc/lib/ui/separator";
+import * as dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { Separator } from '@writeme/wmc/lib/ui/separator';
+import { toast } from '@writeme/wmc/lib/ui/use-toast';
+import {useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
+export interface CommentSectionProps {
+  comments: any;
+  storyId: string;
+  chapterId?: string;
+}
+
+export default function CommentSection({
+  comments,
+  storyId,
+  chapterId,
+}: CommentSectionProps) {
+  dayjs.extend(relativeTime);
+  const [input, setInput] = useState('');
+  const router = useRouter();
+  const { data: session, status } = useSession();
 
 
-export default function CommentSection() {
-    let story: any = { comments : []}
-    story.comments  = [{author: {username: "John Doe", image: "https://avatars.githubusercontent.com/u/72245642?v=4", id: "some long string", createdAt: new Date('December 17, 1995 03:24:00')}, content: "Hello there"}, {author: {username: "John Doe", image: "https://avatars.githubusercontent.com/u/72245642?v=4", id: "some long string", createdAt: new Date('June 13, 2024 03:24:00')}, content: "Hello there"},{author: {username: "John Doe", image: "https://avatars.githubusercontent.com/u/72245642?v=4", id: "some long string"}, content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate, labore maiores, ullam accusantium facere aliquam neque ipsum sit impedit ratione officiis adipisci libero corporis earum optio soluta similique temporibus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate, labore maiores, ullam accusantium facere aliquam neque ipsum sit impedit ratione officiis adipisci libero corporis earum optio soluta similique temporibus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate, labore maiores, ullam accusantium facere aliquam neque ipsum sit impedit ratione officiis adipisci libero corporis earum optio soluta similique temporibus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate, labore maiores, ullam accusantium facere aliquam neque ipsum sit impedit ratione officiis adipisci libero corporis earum optio soluta similique temporibus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptate, labore maiores, ullam accusantium facere aliquam neque ipsum sit impedit ratione officiis adipisci libero corporis earum optio soluta similique temporibus!", createdAt: new Date('June 13, 2025 03:24:00')}]
-    dayjs.extend(relativeTime)
+  const handleComment = async () => {
+    try {
+      const res = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+            content: input,
+            storyId: storyId,
+            chapterId: chapterId
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
+      if (!res.ok) {
+        const errorData = await res.json();
 
-    return (
-        <Card className="w-full">
-            <CardHeader className="bg-muted/50">
-                <CardTitle className="flex gap-2"><MessageSquare></MessageSquare> Comments</CardTitle>
-            </CardHeader>
-            <Separator></Separator>
-            <CardContent className="flex flex-col">
-                {story.comments.map(c => (
-                    <div className="flex gap-2 items-start justify-start">
-                        <Avatar className="mt-3">
-                            <AvatarImage src={c.author.image} />
-                            <AvatarFallback>{c.author.username}</AvatarFallback>
-                        </Avatar>
-                        <div>
-                            <div className="flex items-center gap-1">
-                                <Link href={`/user/${c.author.id}`} className={cn(buttonVariants({variant: "link"}), "p-0")}>{c.author.username}</Link>
-                                <span>·</span>
-                                <span className="text-primary text-sm">{dayjs(c.createdAt).fromNow()}</span>
-                            </div>
-                            <p className="rounded-tr-xl rounded-br-xl rounded-bl-2xl py-2 px-4 bg-accent w-fit">{c.content}</p>
-                        </div>
-                    </div>
-                ))}
-            </CardContent>
-            <Separator></Separator>
-            {/* TODO: wrap in session check */}
-            <CardFooter className="flex justify-center items-center pt-6">
-                <div className="flex items-center gap-2 w-full justify-between">
-                    <Button variant="outline" size="icon">
-                        <Smile />
-                    </Button>
-                    <Input className="grow w-full" type="text" placeholder="let us know your thoughts..." />
-                    <Button variant="outline" size="icon"> 
-                        <Send />    
-                    </Button>
-                </div>
-            </CardFooter>
-        </Card>
-    )
+        if (Array.isArray(errorData.errors) && errorData.errors.length > 0) {
+          errorData.errors.forEach((error: any) => {
+            toast({
+              title: error.message,
+              variant: 'destructive'
+            })
+          });
+
+          return;
+        }
+
+        toast({
+          title: errorData.message,
+          variant: 'destructive'
+        })
+        return;
+      }else {
+        const data = await res.json();
+        router.refresh();
+        setInput("")
+        toast({
+            title: "Comment Created!"
+        })
+      }
+
+    } catch (e: any) {}
+  };
+
+  return (
+    <Card className="w-full">
+      <CardHeader className="bg-muted/50">
+        <CardTitle className="flex gap-2">
+          <MessageSquare></MessageSquare> Comments
+        </CardTitle>
+      </CardHeader>
+      <Separator></Separator>
+      <CardContent className="flex flex-col">
+        {comments.length == 0 ? <><span className="grow text-center items-center mt-4" >Be the first to comment.</span></>: comments.map((c) => (
+          <div className="flex gap-2 items-start justify-start">
+            <Avatar className="mt-3">
+              <AvatarImage src={c.author.image} />
+              <AvatarFallback>{c.author.username}</AvatarFallback>
+            </Avatar>
+            <div>
+              <div className="flex items-center gap-1">
+                <Link
+                  href={`/user/${c.author.id}`}
+                  className={cn(buttonVariants({ variant: 'link' }), 'p-0')}
+                >
+                  {c.author.username}
+                </Link>
+                <span>·</span>
+                <span className="text-primary text-sm">
+                  {dayjs(c.createdAt).fromNow()}
+                </span>
+              </div>
+              <p className="rounded-tr-xl rounded-br-xl rounded-bl-2xl py-2 px-4 bg-accent w-fit">
+                {c.content}
+              </p>
+            </div>
+          </div>
+        ))}
+
+      </CardContent>
+      <Separator></Separator>
+      {/* TODO: wrap in session check */}
+      <CardFooter className="flex justify-center items-center pt-6">
+        { status == "authenticated" ? 
+        <div className="flex items-center gap-2 w-full justify-between">
+          <Button variant="outline" size="icon">
+            {/* <EmojiPicker></EmojiPicker> */}
+            <Smile> </Smile>
+          </Button>
+          <Input
+            fill={true}
+            onChange={(value) => setInput(value.target.value)}
+            value={input}
+            className="grow w-full"
+            type="text"
+            placeholder="let us know your thoughts..."
+          />
+          <Button variant="outline" size="icon" onClick={handleComment}>
+            <Send />
+          </Button>
+        </div> : <span className="grow text-center">Log In to Comment</span>}
+      </CardFooter>
+    </Card>
+  );
 }
