1c915548ea6e8c672ab5899d1fd2ac75b77eee72
CostaEllinas <u21455903@tuks.co.za>
2024-06-21 17:34:03 +0200

    fix(schema): duplicate entries

--- apps/writeme/db/user-schema.ts
+++ apps/writeme/db/user-schema.ts
@@ -61,17 +61,6 @@ export const updateUserSchemaOAuth = object({
 // photo: string().optional(),
 })
 
-export const updateUserSchemaOAuth = object({
-  name: string({ required_error: 'Name is required' }).min(
-    1,
-    'Name is required'
-  ),
-  email: string({ required_error: 'Email is required' })
-    .min(1, 'Email is required')
-    .email('Invalid email'),
-  bio: string().optional(),
-// photo: string().optional(),
-})
 
 export const followerUserSchema = object({
   userId: string({ required_error: 'User ID required'})
