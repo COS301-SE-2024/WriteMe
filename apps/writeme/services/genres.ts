21ab6d51a31a4ab2cefedff9a850286b138f452e
MattPret12 <u21775169@tuks.co.za>
2024-06-22 11:24:19 +0200

    fix(services): removed genres for now

--- apps/writeme/services/genres.ts
+++ apps/writeme/services/genres.ts
@@ -1,7 +1,7 @@
-import { db } from "../db/db";
-import { genres } from "../db/schema";
-
-export async function getAllGenres(){
-    let result = await db.select({value: genres.id, label: genres.genre}).from(genres);
-    return result;
-}
+// import { db } from "../db/db";
+// import { genres } from "../db/schema";
+//
+// export async function getAllGenres(){
+//     let result = await db.select({value: genres.id, label: genres.genre}).from(genres);
+//     return result;
+// }
