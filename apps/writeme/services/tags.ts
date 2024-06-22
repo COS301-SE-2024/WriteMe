3cd0e7f5348e226a11ce539135264304a16d9f7b
MattPret12 <u21775169@tuks.co.za>
2024-06-22 11:21:33 +0200

    fix(services): removed tags

--- apps/writeme/services/tags.ts
+++ apps/writeme/services/tags.ts
@@ -1,7 +1,7 @@
-import { db } from "../db/db";
-import { tags } from "../db/schema";
-
-export async function getAllTags(){
-    let result = await db.select({value: tags.id, label: tags.tag}).from(tags);
-    return result;
-}
+// import { db } from "../db/db";
+// import { tags } from "../db/schema";
+//
+// export async function getAllTags(){
+//     let result = await db.select({value: tags.id, label: tags.tag}).from(tags);
+//     return result;
+// }
