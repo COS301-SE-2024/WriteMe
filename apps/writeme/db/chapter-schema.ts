0a3d91f33a756cd8050853beb51409c3bab0e4a0
MattPret12 <u21775169@tuks.co.za>
2024-06-22 11:22:29 +0200

    feat(schema): updated chapters schema to include edit schema

--- apps/writeme/db/chapter-schema.ts
+++ apps/writeme/db/chapter-schema.ts
@@ -1,4 +1,5 @@
-import { array, boolean, object, string, z } from 'zod';
+import { array, boolean, object, string, TypeOf, z } from 'zod';
+import { loginUserSchema } from './user-schema';
 
 
 export const createChapterSchema = object({
@@ -20,3 +21,13 @@ export const updateChapterSchema = object({
   blocks: array(z.any(), {}),
   published: boolean({required_error: "published needs to be present"}),
 })
+
+
+export const editChapterSchema = object({
+  title: string({required_error: "Title is Required"}).max(255, "Title is too long, a maximum of 255 characters are allowed").min(3, 'title should be at least 3 characters long.'),
+  brief: string({required_error: "brief is required"}).max(40, "Brief is too long, a maximum of 40 characters are allowed. It's meant to challenge you...").optional(),
+  description: string({required_error: "description is required"}).max(10000, "Description is too long, maximum of 10000 characters").optional(),
+  published: boolean({required_error: "published needs to be present"}),
+})
+
+export type EditChapterInput = TypeOf<typeof editChapterSchema>;
