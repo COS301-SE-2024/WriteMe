2306c848755cf4a9b978b840a00e82bd27699ecc
Yashvitha <kyashvitha@gmail.com>
2024-07-12 22:19:31 +0200

    feat(pages): make login and signup pages responsive

--- apps/writeme/app/auth/signup/page.tsx
+++ apps/writeme/app/auth/signup/page.tsx
@@ -1,4 +1,4 @@
-import {SignupFormDemo} from '@writeme/wmc/lib/ui/signup-form';
+import { SignupFormDemo } from '@writeme/wmc/lib/ui/signup-form';
 import LocalNavbar from '@writeme/wmc/lib/ui/local-navbar';
 // import WriteMeLogo from "../assets/WriteMe.png";
 
@@ -7,9 +7,13 @@ export interface MyworksProps {}
 
 export default function SignUp(props: MyworksProps) {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <LocalNavbar />
-      <SignupFormDemo />
+      <div className="flex-grow flex items-center justify-center">
+        <div className="w-full max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+          <SignupFormDemo />
+        </div>
+      </div>
     </div>
   );
 }
