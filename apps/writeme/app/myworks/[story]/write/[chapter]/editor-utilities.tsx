6627e21513ec17394bafdf94b7fbaa8caef26831
Yashvitha <kyashvitha@gmail.com>
2024-08-03 23:52:59 +0200

    fix(pages): update editor-utilities

--- apps/writeme/app/myworks/[story]/write/[chapter]/editor-utilities.tsx
+++ apps/writeme/app/myworks/[story]/write/[chapter]/editor-utilities.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import PromptPad from './prompt-pad';
 import { CardStack } from '@writeme/wmc/lib/ui/card-stack';
 
@@ -14,12 +14,20 @@ export const UtilContext = React.createContext({
 export default  function EditorUtils() {
     const [promptPadContent, setPromptPadContent] = useState("");
     const [suggestionCards, setSuggestionCards] = useState([]);
+
+
+    const [currentCards , setCurrentCards] = useState([])
+    useEffect(() => {
+        setCurrentCards(suggestionCards)
+        console.log(suggestionCards)
+    }, [suggestionCards])
+
     return (
 
         // @ts-ignore
         <UtilContext.Provider value={{promptPadContent, setPromptPadContent, suggestionCards, setSuggestionCards}}>
             <>
-            {suggestionCards.length > 0 ? <CardStack items={suggestionCards.map((c, idx) => ({
+            {currentCards.length > 0 ? <CardStack items={currentCards.map((c, idx) => ({
                 id: idx,
                 name: c.q,
                 designation: c.a,
