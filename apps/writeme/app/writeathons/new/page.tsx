968effb70e22af10c447011d07df33189dc33a2a
MattPret12 <u21775169@tuks.co.za>
2024-09-26 14:33:58 +0200

    feat(writeathons): updated writeathons to allow for times as well

--- apps/writeme/app/writeathons/new/page.tsx
+++ apps/writeme/app/writeathons/new/page.tsx
@@ -1,24 +1,30 @@
-'use client'
+'use client';
 
-import { Card, CardContent, CardHeader, CardTitle } from '@writeme/wmc'
-import AutoForm, { AutoFormSubmit } from '@writeme/wmc/lib/ui/auto-form'
-import LocalNavbar from '@writeme/wmc/lib/ui/local-navbar'
-import { writeathonSchema } from 'apps/writeme/db/story-schema'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '@writeme/wmc';
+import AutoForm, { AutoFormSubmit } from '@writeme/wmc/lib/ui/auto-form';
+import LocalNavbar from '@writeme/wmc/lib/ui/local-navbar';
+import { writeathonSchema } from 'apps/writeme/db/story-schema';
+import { useRouter } from 'next/navigation';
+import React from 'react';
 import { toast } from '@writeme/wmc/lib/ui/use-toast';
 
 const Writeathons = () => {
   const router = useRouter();
 
-  const onWriteathonCreate = async (title: string, description: string, brief: string, startDate: Date, endDate: Date) => {
+  const onWriteathonCreate = async (
+    title: string,
+    description: string,
+    brief: string,
+    startDate: Date,
+    endDate: Date
+  ) => {
     const values = {
       title: title,
       description: description,
       brief: brief,
       startDate: startDate,
-      endDate: endDate
-    }
+      endDate: endDate,
+    };
 
     try {
       const res = await fetch('/api/writeathons', {
@@ -36,8 +42,8 @@ const Writeathons = () => {
           errorData.errors.forEach((error: any) => {
             toast({
               title: error.message,
-              variant: 'destructive'
-            })
+              variant: 'destructive',
+            });
           });
 
           return;
@@ -45,58 +51,60 @@ const Writeathons = () => {
 
         toast({
           title: errorData.message,
-          variant: 'destructive'
-        })
+          variant: 'destructive',
+        });
         return;
       }
       toast({
         title: 'Writeathon Created',
-        variant: "default"
-      })
+        variant: 'default',
+      });
 
       router.push(`/writeathons`);
-
     } catch (error: any) {
       toast({
         title: error.message,
-        variant: 'destructive'
-      })
+        variant: 'destructive',
+      });
     } finally {
-      
     }
-  }
-  
+  };
+
   return (
     <>
       <LocalNavbar />
       <Card className="mx-auto max-w-sm mt-8">
         <CardHeader>
-            <CardTitle className="text-2xl">Create a Writeathon</CardTitle>
-          </CardHeader>
+          <CardTitle className="text-2xl">Create a Writeathon</CardTitle>
+        </CardHeader>
         <CardContent>
-            <AutoForm
-              onSubmit={(data) => {
-                onWriteathonCreate(data.title, data.description, data.brief, data.startDate as Date, data.endDate as Date)
-              }}
-              formSchema={writeathonSchema}
-              values={{
-                
-              }}
-              fieldConfig={{
-               startDate: {
-                fieldType: 'date'
-               },
-               endDate: {
-                fieldType: 'date'
-               }
-              }}
-              >
-              <AutoFormSubmit>Create</AutoFormSubmit>
-            </AutoForm>
+          <AutoForm
+            onSubmit={(data) => {
+              onWriteathonCreate(
+                data.title,
+                data.description,
+                data.brief,
+                data.startDate as Date,
+                data.endDate as Date
+              );
+            }}
+            formSchema={writeathonSchema}
+            values={{}}
+            fieldConfig={{
+              startDate: {
+                fieldType: 'datetime',
+              },
+              endDate: {
+                fieldType: 'datetime',
+              },
+            }}
+          >
+            <AutoFormSubmit>Create</AutoFormSubmit>
+          </AutoForm>
         </CardContent>
       </Card>
     </>
-  )
-}
+  );
+};
 
-export default Writeathons
+export default Writeathons;
